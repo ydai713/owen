a2b169a8e46c11bbc30b613f7eca60144b3ee3b9
Yang Dai <yang.dai@klaviyo.com>
2024-06-28 18:07:14 -0700

    update

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+"""
+The way to use while loop in python:
+
+while condition:
+    <code block>
+"""
+
+# challenge: print hello world 5 times using while loop
+
+i = 0
+while i < 5:
+    print("hello world")
+    i = i + 1
