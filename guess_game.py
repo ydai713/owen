ef53d400b957d17f706b70c7cfb8df0c609ed423
Yang Dai <yang.dai@klaviyo.com>
2024-07-12 16:55:34 -0700

    create guess game

--- guess_game.py
+++ guess_game.py
@@ -5,16 +5,21 @@ computer will have a correct number. It will ask a player to put a number.
 If the number matches, then the game is over and the player wins.
 if the number does not match, computer will ask the player to put another number.
 
-hint: use while loop
 """
+import random
+
 
 print("##########################################")
 print("           Welcome to the game")
 print("##########################################")
 
-correct_number = 5
+correct_number = random.randint(1, 10)
 player_number = input("Put a number: ")
 player_number = int(player_number)
 
-if correct_number == player_number:
-    print("hooray, you win!")
+while correct_number != player_number:
+    print('Sorry, try again')
+    player_number = input('retry: ')
+    player_number = int(player_number)
+
+print('Congratulation, you win the game')
---
+++ test.py
@@ -0,0 +1,5 @@
+import random
+
+
+correct_number = random.randint(50, 100)
+print(correct_number)
